1043110f240687d26495fd569c15e02c9487cecf
MaksymHr <maksim.hryschenko@gmail.com>
2022-04-12 18:15:00 +0300

    add html1

---
+++ index.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ * Lesson 1 HTML Основные понятия.
+ * Темы урока
+ * Понятие тега. Структура HTML страниц
+ * Параграфы, заголовки, комментарии
+ * Атрибуты тегов
+ * Изображения. Форматы изображения в Web
+ * Списки
+ * */
+ header('Content-Type: text/html; charset=utf-8');
+$request = $_SERVER['REQUEST_URI'];
+
+
+if( preg_match("/^\/task-(\d+)\/$/", $request, $match) ) {
+    
+    // страницы с заданиями
+    $task_id = $match[1];
+    if(!$task_id) {
+        header( 'Location: /404/', true, 301 );
+        exit();
+    }  
+    
+    $path = "tasks/task-{$task_id}/index-{$task_id}.html";
+    
+    if(file_exists($path)) {
+        
+        include $path;
+    } else die('file error');
+    
+    
+} else {
+    
+    // остальные страницы
+    switch($request) {
+
+        case '/'            : include "www/index.html"; break;
+        case '/task-list/'  : include "www/task_list.html"; break;
+        default             : include "www/404.html"; break;
+    }
+    
+}
+
+
+
+
+die();
+
+
+
+
